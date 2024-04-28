087c509c6d773ee83e1177f8770c59e2f907966a
seif khaled <seifkhaledabdelsalam@gmail.com>
2024-04-28 12:45:31 +0300

    second edit

--- cpp test project/cpp test project/cpp test project.cpp
+++ cpp test project/cpp test project/cpp test project.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 using namespace std;
+
 int main() {
-	cout << "hello world of test gi gui";
+
+	cout << "hello world of test gi gui"<<endl;
+	cout << "hello world of test gi gui"<<endl;
+	return 0;
 }

aa108a037b1228d7234896bd42382493fd3b32c8
seif khaled <seifkhaledabdelsalam@gmail.com>
2024-04-28 18:47:02 +0300

    testing first change

--- cpp test project/cpp test project/cpp test project.cpp
+++ cpp test project/cpp test project/cpp test project.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
 
 	cout << "hello world of test gi gui"<<endl;
-	cout << "hello world of test gi gui"<<endl;
+	cout << "hello world of test gi gui" << endl;
+	cout << "first change" << endl;
 	return 0;
 }
